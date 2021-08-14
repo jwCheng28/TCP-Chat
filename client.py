f0d44228aaf6ffa6fd847573fbe740e3f17d29f9
jwCheng28 <jwCheng28@users.noreply.github.com>
2021-08-13 23:28:31 -0700

    Updated client to allow contiously send and recieve msg until QUIT

--- client.py
+++ client.py
@@ -1,12 +1,18 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = "127.0.0.1"
+host = socket.gethostname()
 port = 45001
 
 s.connect((host, port))
-msg = input()
-s.send(msg.encode())
-#resp = s.recv(64)
-#print("Message Received: {}".format(resp))
+user = input("Enter Username: ")
+s.send(user.encode())
+while True:
+    msg = input("Enter MSG: ")
+    s.send(msg.encode())
+    if msg == "QUIT":
+        break
+    response = [e.strip() for e in s.recv(300).decode().split(":")]
+    respUser, respMsg = response[0], response[1]
+    print("{}: {}".format(respUser, respMsg))
 s.close()
