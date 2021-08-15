78b346036ecf884a035575390d4863ff2e550b91
jwCheng28 <chengjw68@gmail.com>
2021-08-15 14:40:53 -0700

    Added some comments and prints/send messages for a more intuitive interface

--- server.c
+++ server.c
@@ -33,6 +33,11 @@ static clientConn clients[MAX_CLIENT];
 static int clientCount = 0;
 
 int availableSlot() {
+    /* Functionality:
+     *  - Search through client pool to see if there's an available slot for a new client
+     * Return:
+     *  - Returns index of available slot or -1 if there's no available slot
+     */
     if (clientCount >= MAX_CLIENT) return -1;
     for (int i = 0; i < MAX_CLIENT; ++i) {
         if (clients[i].occupied != 1) return i;
@@ -41,6 +46,11 @@ int availableSlot() {
 }
 
 void removeClient(clientConn *client) {
+    /* Functionality:
+     *  - Used to clean the values inside a given clientConn struct
+     * Params:
+     *  - clientConn *client : clientConn struct object that we want to remove
+     */
     client->servSocket = -1;
     client->connSocket = -1;
     client->occupied = 0;
@@ -49,28 +59,46 @@ void removeClient(clientConn *client) {
 }
 
 void sendToAllClient(char response[RESPSIZE]) {
+    /* Functionality:
+     *  - Send a given response to every connected client
+     * Params:
+     *  - char response[RESPSIZE] : the response that we want to send to all the client
+     */
     for (int i = 0; i < MAX_CLIENT; ++i) {
         if (clients[i].occupied != 1) continue;
         send(clients[i].connSocket, response, strlen(response), 0);
     }
 }
 
-void sendToAllButSelf(char response[RESPSIZE], int selfIndex) {
+void sendToAllButIndex(char response[RESPSIZE], int index) {
+    /* Functionality:
+     *  - Send a given response to every connected client except the client with the given index
+     * Params:
+     *  - char response[RESPSIZE] : the response that we want to send
+     *  - int selfIndex : specific client index
+     */
     for (int i = 0; i < MAX_CLIENT; ++i) {
-        if (clients[i].occupied != 1 || i == selfIndex) continue;
+        if (clients[i].occupied != 1 || i == index) continue;
         send(clients[i].connSocket, response, strlen(response), 0);
     }
 }
 
 void *handleClient(void *args) {
+    /* Functionality:
+     *  - Thread function that handles a particular client
+     *  - Logs the client activity and broadcase client message to all the other clients
+     */
     clientConn *client = (clientConn *) args;
     int servSocket = client->servSocket;
     int connSocket = client->connSocket;
     
     char response[RESPSIZE], buffer[BUFSIZE], user[UNAMESIZE], key[KEYSIZE];
     memset(user, 0, UNAMESIZE);
+    memset(response, 0, RESPSIZE);
     recv(connSocket, user, UNAMESIZE-1, 0);
     printf("Connection Log - %s Connected\n", user);
+    sprintf(response, "> %s has joined", user);
+    sendToAllButIndex(response, client->index);
     while (1) {
         memset(response, 0, RESPSIZE);
         memset(buffer, 0, BUFSIZE);
@@ -78,13 +106,14 @@ void *handleClient(void *args) {
         recv(connSocket, buffer, BUFSIZE-1, 0);
         memcpy(key, buffer, KEYSIZE-1);
         if (strcmp(key, "QUIT")==0) {
+            sprintf(response, "> %s has disconnected", user);
+            sendToAllButIndex(response, client->index);
             removeClient(client);
             break;
         }
         printf("Message Log - %s: %s\n", user, buffer);
         sprintf(response, "%s : %s", user, buffer);
-        sendToAllButSelf(response, client->index);
-        // sendToAllClient(response);
+        sendToAllButIndex(response, client->index);
     }
     printf("%s Has Disconnected\n", user);
     close(connSocket);
@@ -97,12 +126,14 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    // Create server socket
     int servSocket = socket(AF_INET, SOCK_STREAM, 0), connSocket;
     if (servSocket < 0) {
         printf("Server Socket Open Failed\n");
         return 1;
     }
 
+    // Initialize server information and bind server
     struct sockaddr_in servAddr;
     initServerAddr(&servAddr, atoi(argv[1]));
     if (bind(servSocket, (struct sockaddr *) &servAddr, sizeof(servAddr)) < 0) {
@@ -110,6 +141,8 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    printf("Server had started and is now running\n");
+    // Start listening and handle clients
     listen(servSocket, 5);
     while (1) {
         struct sockaddr_in clientAddr;
